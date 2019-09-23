94df515c66107093d029d3b41f993a289ae8dcf8
rodionovv <wasyarod@mail.ru>
2019-09-24 01:54:02 +0300

    for codemagic

--- lib/main.dart
+++ lib/main.dart
@@ -1,153 +1,56 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart' show debugPaintSizeEnabled;
 
-
+void main() {
+  debugPaintSizeEnabled = false; // Set to true for visual layout
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter layout demo',
-      home: buildHomePage('Strawberry Pavlova Recipe'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Flutter layout demo'),
+        ),
+        body: Center(child: _buildImageColumn()),
+      ),
     );
   }
-  Widget buildHomePage(String title){
-    final titleText = Container(
-      padding: EdgeInsets.all(20),
-      child: Text(
-        'Strwaberry Pavlova',
-        style: TextStyle(
-          fontWeight: FontWeight.w800,
-          letterSpacing: 0.5,
-          fontSize: 30,
-        ),
-      ),
-    );
 
-    final subTitle = Text(
-      'Pavlova is a meringue-based dessert named after the Russian ballerina '
-          'Anna Pavlova. Pavlova features a crisp crust and soft, light inside, '
-          'topped with fruit and whipped cream.',
-      textAlign: TextAlign.center,
-      style: TextStyle(
-        fontFamily: 'Georgia',
-        fontSize: 25,
-      ),
-    );
-
-    var stars = Row(
-      mainAxisSize: MainAxisSize.min,
-      children: <Widget>[
-        Icon(Icons.star, color: Colors.green[500]),
-        Icon(Icons.star, color: Colors.green[500]),
-        Icon(Icons.star, color: Colors.green[500]),
-        Icon(Icons.star, color: Colors.black),
-        Icon(Icons.star, color: Colors.black),
+  // #docregion column
+  Widget _buildImageColumn() => Container(
+    decoration: BoxDecoration(
+      color: Colors.black26,
+    ),
+    child: Column(
+      children: [
+        _buildImageRow(1),
+        _buildImageRow(3),
       ],
-    );
+    ),
+  );
+  // #enddocregion column
 
-    final ratings = Container(
-      padding: EdgeInsets.all(20),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: <Widget>[
-          stars,
-          Text(
-            '170 Reviews',
-            style: TextStyle(
-              color: Colors.black,
-              fontWeight: FontWeight.w800,
-              fontFamily: 'Roboto',
-              letterSpacing: 0.5,
-              fontSize: 20,
-            ),
-          )
-        ],
+  // #docregion row
+  Widget _buildDecoratedImage(int imageIndex) => Expanded(
+    child: Container(
+      decoration: BoxDecoration(
+        border: Border.all(width: 10, color: Colors.black38),
+        borderRadius: const BorderRadius.all(const Radius.circular(8)),
       ),
-    );
+      margin: const EdgeInsets.all(4),
+      child: Image.asset('images/pic$imageIndex.jpg'),
+    ),
+  );
 
-    final descTextStyle = TextStyle(
-      color: Colors.black,
-      fontWeight: FontWeight.w800,
-      fontFamily: 'Roboto',
-      letterSpacing: 0.5,
-      fontSize: 18,
-      height: 2,
-    );
-
-    final iconList = DefaultTextStyle.merge(
-        style: descTextStyle,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: <Widget>[
-            Column(
-              children: <Widget>[
-                Icon(Icons.kitchen, color: Colors.green[500]),
-                Text('PREP:'),
-                Text('25 min'),
-              ],
-            ),
-            Column(
-              children: <Widget>[
-                Icon(Icons.timer, color: Colors.green[500]),
-                Text('COOK:'),
-                Text('1 hr'),
-              ],
-            ),
-            Column(
-              children: <Widget>[
-                Icon(Icons.restaurant, color: Colors.green[500]),
-                Text('FEEDS:'),
-                Text('4-6'),
-              ],
-            ),
-          ],
-        )
-    );
-
-    final leftColumn = Container(
-      padding: EdgeInsets.fromLTRB(20, 30, 20, 20),
-      child: Column(
-        children: <Widget>[
-          titleText,
-          subTitle,
-          ratings,
-          iconList
-        ],
-      ),
-    );
-
-    final mainImage = Image.asset(
-      'images/pic1.jpg',
-      fit: BoxFit.cover,
-    );
-
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(title),
-      ),
-
-      body: Center(
-        child: Container(
-          margin: EdgeInsets.fromLTRB(0, 40, 0, 30),
-          height: 600,
-          child: Card(
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Container(
-                  width: 440,
-                  child: leftColumn,
-                ),
-                mainImage,
-              ],
-            ),
-          ),
-        ),
-      ),
-    );
-
-  }
-
+  Widget _buildImageRow(int imageIndex) => Row(
+    children: [
+      _buildDecoratedImage(imageIndex),
+      _buildDecoratedImage(imageIndex + 1),
+    ],
+  );
+// #enddocregion row
 }
-
-void main() => runApp(MyApp());
