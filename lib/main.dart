fc2b1b6cfe3bf56704552c439ae69567b53b35c5
rodionovv <wasyarod@mail.ru>
2019-09-25 15:11:59 +0300

    Stack and card

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,139 @@ void main() {
   runApp(MyApp());
 }
 
+class Pic {
+  String _path;
+  Pic(this._path);
+  path() {
+    return this._path;
+  }
+}
+
+class Body extends StatefulWidget {
+  @override
+  _BodyState createState() => _BodyState();
+}
+
+class _BodyState extends State<Body> {
+  bool showCard = false;
+  Pic pic;
+
+
+  @override
+  Widget build(BuildContext context) {
+    return showCard ? _makeColumn() : GridView.extent(
+      maxCrossAxisExtent: 150,
+      padding: const EdgeInsets.all(4),
+      mainAxisSpacing: 4,
+      crossAxisSpacing: 4,
+      children: _buildGridTileList(16),
+    );
+  }
+
+  List<Stack> _buildGridTileList(int count) => List.generate(
+      count, (i) => _buildStack(Pic('images/pic${i + 1}.jpg')));
+
+
+
+
+  Widget _makeColumn() => Column(
+    children: <Widget>[
+      _buildStack(pic),
+      _buildCard(),
+    ],
+  );
+
+  Widget _buildCard() => SizedBox(
+    height: 210,
+    width: 500,
+    child: GestureDetector(
+     onTap: () {
+       setState(() {
+         showCard = !showCard;
+       });
+     },
+      child: Card(
+        child: Column(
+          children: [
+            ListTile(
+              title: Text('1625 Main Street',
+                  style: TextStyle(fontWeight: FontWeight.w500)),
+              subtitle: Text('My City, CA 99984'),
+              leading: Icon(
+                Icons.restaurant_menu,
+                color: Colors.blue[500],
+              ),
+            ),
+            Divider(),
+            ListTile(
+              title: Text('(408) 555-1212',
+                  style: TextStyle(fontWeight: FontWeight.w500)),
+              leading: Icon(
+                Icons.contact_phone,
+                color: Colors.blue[500],
+              ),
+              trailing: GestureDetector(
+                onTap: () {
+                  setState(() {
+                    pic = Pic("hi");
+                  });
+                },
+                child: Icon(
+                    Icons.more_vert),
+                ),
+              ),
+            ListTile(
+              title: Text('costa@example.com'),
+              leading: Icon(
+                Icons.contact_mail,
+                color: Colors.blue[500],
+              ),
+            ),
+          ],
+        ),
+      ),
+    )
+  );
+  // #enddocregion Card
+
+
+
+  // #docregion Stack
+  Widget _buildStack(Pic image) => Stack(
+    alignment: const Alignment(0.6, 0.6),
+    children: [
+      CircleAvatar(
+        backgroundImage: AssetImage(image.path()),
+        radius: 100,
+      ),
+      GestureDetector(
+        onTap: () {
+         setState(() {
+           showCard = !showCard;
+           pic = image;
+         });
+        },
+        child:  Container(
+          decoration: BoxDecoration(
+            color: Colors.black45,
+          ),
+          child: Text(
+            image.path(),
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+            ),
+          ),
+        ),
+      )
+    ],
+  );
+}
+
 class MyApp extends StatelessWidget {
+  static final showCard = false; // Set to false to show ListView
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -15,42 +147,11 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text('Flutter layout demo'),
         ),
-        body: Center(child: _buildImageColumn()),
+        body: Body(),
       ),
     );
   }
+  // #docregion Card
 
-  // #docregion column
-  Widget _buildImageColumn() => Container(
-    decoration: BoxDecoration(
-      color: Colors.black26,
-    ),
-    child: Column(
-      children: [
-        _buildImageRow(1),
-        _buildImageRow(3),
-      ],
-    ),
-  );
-  // #enddocregion column
-
-  // #docregion row
-  Widget _buildDecoratedImage(int imageIndex) => Expanded(
-    child: Container(
-      decoration: BoxDecoration(
-        border: Border.all(width: 10, color: Colors.black38),
-        borderRadius: const BorderRadius.all(const Radius.circular(8)),
-      ),
-      margin: const EdgeInsets.all(4),
-      child: Image.asset('images/pic$imageIndex.jpg'),
-    ),
-  );
-
-  Widget _buildImageRow(int imageIndex) => Row(
-    children: [
-      _buildDecoratedImage(imageIndex),
-      _buildDecoratedImage(imageIndex + 1),
-    ],
-  );
-// #enddocregion row
+// #enddocregion Stack
 }
