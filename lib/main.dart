1798070ca63610959a02b57d6f04cb8294942147
rodionovv <wasyarod@mail.ru>
2019-09-28 22:25:14 +0300

    adding interactivity

--- lib/main.dart
+++ lib/main.dart
@@ -37,11 +37,7 @@ class MyApp extends StatelessWidget {
               ],
             ),
           ),
-          Icon(
-            Icons.star,
-            color: Colors.red[500],
-          ),
-          Text('41'),
+          FavoriteWidget(),
         ],
       ),
     );
@@ -110,6 +106,51 @@ class MyApp extends StatelessWidget {
               fontWeight: FontWeight.w400,
               color: color,
             ),
+          ),
+        )
+      ],
+    );
+  }
+}
+
+class FavoriteWidget extends StatefulWidget {
+  @override
+  _FavoriteWidgetState createState() => _FavoriteWidgetState();
+}
+
+class _FavoriteWidgetState extends State<FavoriteWidget>{
+  bool _isFavorited = true;
+  int _favoritCount = 41;
+
+  void _toggleFavorite(){
+    setState(() {
+      if (_isFavorited) {
+        _favoritCount -= 1;
+        _isFavorited = false;
+      } else {
+        _favoritCount += 1;
+        _isFavorited = true;
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: <Widget>[
+        Container(
+          padding: EdgeInsets.all(0),
+          child: IconButton(
+            icon: (_isFavorited ? Icon(Icons.star) : Icon(Icons.star_border)),
+            onPressed: _toggleFavorite,
+            color: Colors.red[500],
+          ),
+        ),
+        SizedBox(
+          width: 18,
+          child: Container(
+            child: Text('$_favoritCount'),
           ),
         )
       ],
