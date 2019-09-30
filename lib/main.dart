523c28b98f4edd76ad7620bb9e401a7728083254
vasya <wasyarod@mail.ru>
2019-09-30 16:09:24 +0300

    reverse  and forward animation

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,58 @@
+import 'package:flutter/animation.dart';
 import 'package:flutter/material.dart';
 
+void main() => runApp(LogoApp());
 
+// #docregion AnimatedLogo
+class AnimatedLogo extends AnimatedWidget {
+  AnimatedLogo({Key key, Animation<double> animation})
+      : super(key: key, listenable: animation);
 
-class LogoApp extends StatefulWidget {
-  _LogoAppState createState() => _LogoAppState();
-}
-
-class _LogoAppState extends State<LogoApp> with SingleTickerProviderStateMixin{
-  @override
   Widget build(BuildContext context) {
+    final Animation<double> animation = listenable as Animation<double>;
     return Center(
       child: Container(
         margin: EdgeInsets.symmetric(vertical: 10),
-        height: 300,
-        width: 300,
+        height: animation.value,
+        width: animation.value,
         child: FlutterLogo(),
       ),
     );
   }
 }
+// #enddocregion AnimatedLogo
 
+class LogoApp extends StatefulWidget {
+  _LogoAppState createState() => _LogoAppState();
+}
 
-void main() => runApp(LogoApp());
+class _LogoAppState extends State<LogoApp> with SingleTickerProviderStateMixin {
+  Animation<double> animation;
+  AnimationController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller =
+        AnimationController(duration: const Duration(seconds: 2), vsync: this);
+    animation = Tween<double>(begin: 0, end: 300).animate(controller)
+      ..addStatusListener((status) => print(('$status')))
+      ..addStatusListener((status) {
+        if (status == AnimationStatus.completed){
+          controller.reverse();
+        } else if (status == AnimationStatus.dismissed){
+          controller.forward();
+        }
+      });
+    controller.forward();
+  }
+
+  @override
+  Widget build(BuildContext context) => AnimatedLogo(animation: animation);
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+}
