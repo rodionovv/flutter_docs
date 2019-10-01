c70b452283f539ca1b6b6c638c1d43ff813a929e
rodionovv <wasyarod@mail.ru>
2019-10-01 16:33:34 +0300

    advanced radial animation

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,18 @@
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/scheduler.dart' show timeDilation;
-import 'package:flutter/material.dart';
 import 'dart:math' as math;
 
-
-void main() {
-  runApp(MaterialApp(home: RadialExpansionDemo()));
-}
-
+import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart' show timeDilation;
 
 class Photo extends StatelessWidget {
-  Photo({Key key, this.photo, this.color, this.onTap}) : super(key: key);
+  Photo({ Key key, this.photo, this.color, this.onTap }) : super(key: key);
 
   final String photo;
   final Color color;
   final VoidCallback onTap;
 
-  @override
   Widget build(BuildContext context) {
     return Material(
+      // Slightly opaque color appears where the image has transparency.
       color: Theme.of(context).primaryColor.withOpacity(0.25),
       child: InkWell(
         onTap: onTap,
@@ -38,27 +32,38 @@ class Photo extends StatelessWidget {
 class RadialExpansion extends StatelessWidget {
   RadialExpansion({
     Key key,
+    this.minRadius,
     this.maxRadius,
     this.child,
-  }) : clipRectSize = 2.0 * (maxRadius / math.sqrt2),
-       super(key: key);
+  }) : clipTween = Tween<double>(
+    begin: 2.0 * minRadius,
+    end: 2.0 * (maxRadius / math.sqrt2),
+  ),
+        super(key: key);
 
+  final double minRadius;
   final double maxRadius;
-  final clipRectSize;
+  final Tween<double> clipTween;
   final Widget child;
 
   @override
   Widget build(BuildContext context) {
-    return ClipOval(
-      child: Center(
-        child: SizedBox(
-          width: clipRectSize,
-          height: clipRectSize,
-          child: ClipRect(
-            child: child,
+    return LayoutBuilder(
+      builder: (BuildContext context, BoxConstraints size) {
+        final double t = (size.biggest.width / 2.0 - minRadius) / (maxRadius - minRadius);
+        final double rectClipExtent = clipTween.transform(t);
+        return ClipOval(
+          child: Center(
+            child: SizedBox(
+              width: rectClipExtent,
+              height: rectClipExtent,
+              child: ClipRect(
+                child: child,
+              ),
+            ),
           ),
-        ),
-      ),
+        );
+      },
     );
   }
 }
@@ -68,7 +73,7 @@ class RadialExpansionDemo extends StatelessWidget {
   static const double kMaxRadius = 128.0;
   static const opacityCurve = const Interval(0.0, 0.75, curve: Curves.fastOutSlowIn);
 
-  static RectTween _createREctTween(Rect begin, Rect end) {
+  static RectTween _createRectTween(Rect begin, Rect end) {
     return MaterialRectCenterArcTween(begin: begin, end: end);
   }
 
@@ -80,14 +85,15 @@ class RadialExpansionDemo extends StatelessWidget {
           elevation: 8.0,
           child: Column(
             mainAxisSize: MainAxisSize.min,
-            children: <Widget>[
+            children: [
               SizedBox(
                 width: kMaxRadius * 2.0,
                 height: kMaxRadius * 2.0,
                 child: Hero(
-                  createRectTween: _createREctTween,
+                  createRectTween: _createRectTween,
                   tag: imageName,
                   child: RadialExpansion(
+                    minRadius: kMinRadius,
                     maxRadius: kMaxRadius,
                     child: Photo(
                       photo: imageName,
@@ -111,32 +117,33 @@ class RadialExpansionDemo extends StatelessWidget {
     );
   }
 
-  Widget _buildHero(BuildContext context, String imageName, String description){
+  Widget _buildHero(BuildContext context, String imageName, String description) {
     return Container(
-      width: kMinRadius * 2,
-      height: kMinRadius * 2,
+      width: kMinRadius * 2.0,
+      height: kMinRadius * 2.0,
       child: Hero(
-        createRectTween: _createREctTween,
+        createRectTween: _createRectTween,
         tag: imageName,
         child: RadialExpansion(
+          minRadius: kMinRadius,
           maxRadius: kMaxRadius,
           child: Photo(
             photo: imageName,
             onTap: () {
               Navigator.of(context).push(
                 PageRouteBuilder<void>(
-                  pageBuilder: (BuildContext context, Animation<double> animation, Animation<double> swcondaryAnimation) {
+                  pageBuilder: (BuildContext context, Animation<double> animation, Animation<double> secondaryAnimation) {
                     return AnimatedBuilder(
-                      animation: animation,
-                      builder: (BuildContext context, Widget child) {
-                        return Opacity(
-                          opacity: opacityCurve.transform(animation.value),
-                          child: _buildPage(context, imageName, description),
-                        );
-                      },
+                        animation: animation,
+                        builder: (BuildContext context, Widget child) {
+                          return Opacity(
+                            opacity: opacityCurve.transform(animation.value),
+                            child: _buildPage(context, imageName, description),
+                          );
+                        }
                     );
-                  }
-                )
+                  },
+                ),
               );
             },
           ),
@@ -147,18 +154,18 @@ class RadialExpansionDemo extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    timeDilation = 5.0;
+    timeDilation = 15.0; // 1.0 is normal animation speed.
 
     return Scaffold(
       appBar: AppBar(
         title: const Text('Radial Transition Demo'),
       ),
       body: Container(
-        padding: const EdgeInsets.all(32),
+        padding: const EdgeInsets.all(32.0),
         alignment: FractionalOffset.bottomLeft,
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: <Widget>[
+          children: [
             _buildHero(context, 'images/chair-alpha.png', 'Chair'),
             _buildHero(context, 'images/binoculars-alpha.png', 'Binoculars'),
             _buildHero(context, 'images/beachball-alpha.png', 'Beach ball'),
@@ -167,5 +174,8 @@ class RadialExpansionDemo extends StatelessWidget {
       ),
     );
   }
+}
 
+void main() {
+  runApp(MaterialApp(home: RadialExpansionDemo()));
 }
