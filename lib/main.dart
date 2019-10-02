183efb6c44261802ab510c27d5fffed9c622ec7e
rodionovv <wasyarod@mail.ru>
2019-10-02 11:32:48 +0300

    infiite list with random colors

--- lib/main.dart
+++ lib/main.dart
@@ -1,153 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:random_color/random_color.dart';
 
 
 
 class MyApp extends StatelessWidget {
+
+  Colors geRandomColor() {
+
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter layout demo',
-      home: buildHomePage('Strawberry Pavlova Recipe'),
-    );
-  }
-  Widget buildHomePage(String title){
-    final titleText = Container(
-      padding: EdgeInsets.all(20),
-      child: Text(
-        'Strwaberry Pavlova',
-        style: TextStyle(
-          fontWeight: FontWeight.w800,
-          letterSpacing: 0.5,
-          fontSize: 30,
-        ),
-      ),
-    );
-
-    final subTitle = Text(
-      'Pavlova is a meringue-based dessert named after the Russian ballerina '
-          'Anna Pavlova. Pavlova features a crisp crust and soft, light inside, '
-          'topped with fruit and whipped cream.',
-      textAlign: TextAlign.center,
-      style: TextStyle(
-        fontFamily: 'Georgia',
-        fontSize: 25,
-      ),
-    );
-
-    var stars = Row(
-      mainAxisSize: MainAxisSize.min,
-      children: <Widget>[
-        Icon(Icons.star, color: Colors.green[500]),
-        Icon(Icons.star, color: Colors.green[500]),
-        Icon(Icons.star, color: Colors.green[500]),
-        Icon(Icons.star, color: Colors.black),
-        Icon(Icons.star, color: Colors.black),
-      ],
-    );
-
-    final ratings = Container(
-      padding: EdgeInsets.all(20),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: <Widget>[
-          stars,
-          Text(
-            '170 Reviews',
-            style: TextStyle(
-              color: Colors.black,
-              fontWeight: FontWeight.w800,
-              fontFamily: 'Roboto',
-              letterSpacing: 0.5,
-              fontSize: 20,
-            ),
-          )
-        ],
-      ),
-    );
-
-    final descTextStyle = TextStyle(
-      color: Colors.black,
-      fontWeight: FontWeight.w800,
-      fontFamily: 'Roboto',
-      letterSpacing: 0.5,
-      fontSize: 18,
-      height: 2,
-    );
-
-    final iconList = DefaultTextStyle.merge(
-        style: descTextStyle,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: <Widget>[
-            Column(
-              children: <Widget>[
-                Icon(Icons.kitchen, color: Colors.green[500]),
-                Text('PREP:'),
-                Text('25 min'),
-              ],
-            ),
-            Column(
-              children: <Widget>[
-                Icon(Icons.timer, color: Colors.green[500]),
-                Text('COOK:'),
-                Text('1 hr'),
-              ],
-            ),
-            Column(
-              children: <Widget>[
-                Icon(Icons.restaurant, color: Colors.green[500]),
-                Text('FEEDS:'),
-                Text('4-6'),
-              ],
-            ),
+      home: Scaffold(
+        body: CustomScrollView(
+          slivers: <Widget>[
+            SliverList(
+              delegate: SliverChildBuilderDelegate((BuildContext context, int index) {
+                RandomColor _randomColor = RandomColor();
+                Color _color = _randomColor.randomColor();
+                return Container(color: _color, height: 150
+                ,child: Center(child: Text(_color.toString())));
+              }),
+            )
           ],
-        )
-    );
-
-    final leftColumn = Container(
-      padding: EdgeInsets.fromLTRB(20, 30, 20, 20),
-      child: Column(
-        children: <Widget>[
-          titleText,
-          subTitle,
-          ratings,
-          iconList
-        ],
-      ),
-    );
-
-    final mainImage = Image.asset(
-      'images/pic1.jpg',
-      fit: BoxFit.cover,
-    );
-
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(title),
-      ),
-
-      body: Center(
-        child: Container(
-          margin: EdgeInsets.fromLTRB(0, 40, 0, 30),
-          height: 600,
-          child: Card(
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Container(
-                  width: 440,
-                  child: leftColumn,
-                ),
-                mainImage,
-              ],
-            ),
-          ),
         ),
-      ),
+      )
     );
-
   }
-
 }
 
 void main() => runApp(MyApp());
