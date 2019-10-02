44a376336e323d809b9ddaf2498c0b5a615aad9e
rodionovv <wasyarod@mail.ru>
2019-10-02 12:45:03 +0300

    evrything together

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:random_color/random_color.dart';
+import 'dart:math' as math;
 
 
 
@@ -12,26 +13,127 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        body: CustomScrollView(
-          slivers: <Widget>[
-            SliverGrid(
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 4),
-              delegate: SliverChildBuilderDelegate(
-                  (BuildContext context, int index) {
-                    RandomColor _randomColor = RandomColor();
-                    Color _color = _randomColor.randomColor();
-                    return Container(
-                      color: _color,
-                      height: 150,
-                    );
-                  }
-              ),
-            )
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text('Collapsing List Demo'),
+          ),
+          body: CollapsingList(),
+        )
+    );
+  }
+}
+
+class CollapsingList extends StatelessWidget {
+    SliverPersistentHeader makeHeader(String headerText) {
+      return SliverPersistentHeader(
+        pinned: false,
+        delegate: _SliverAppBarDelegate(
+          minHeight: 0,
+          maxHeight: 200,
+          child: Container(
+            color: Colors.lightBlue,
+            child: Center(
+              child: Text(headerText),
+            ),
+          )
+        ),
+      );
+    }
+
+    @override
+  Widget build(BuildContext context) {
+    return CustomScrollView(
+      slivers: <Widget>[
+        makeHeader('Header Section 1'),
+        SliverGrid.count(
+          crossAxisCount: 3,
+          children: <Widget>[
+            Container(color: Colors.red, height: 150.0),
+            Container(color: Colors.purple, height: 150.0),
+            Container(color: Colors.green, height: 150.0),
+            Container(color: Colors.orange, height: 150.0),
+            Container(color: Colors.yellow, height: 150.0),
+            Container(color: Colors.pink, height: 150.0),
+            Container(color: Colors.cyan, height: 150.0),
+            Container(color: Colors.indigo, height: 150.0),
+            Container(color: Colors.blue, height: 150.0),
           ],
         ),
-      )
+        makeHeader('Header Section 2'),
+        SliverFixedExtentList(
+          itemExtent: 150,
+          delegate: SliverChildListDelegate(
+            [
+              Container(color: Colors.red),
+              Container(color: Colors.purple),
+              Container(color: Colors.green),
+              Container(color: Colors.orange),
+              Container(color: Colors.yellow),
+            ],
+          ),
+        ),
+        makeHeader('Header Section 3'),
+        SliverGrid(
+          gridDelegate: new SliverGridDelegateWithMaxCrossAxisExtent(
+            maxCrossAxisExtent: 200,
+            mainAxisSpacing: 10,
+            crossAxisSpacing: 10,
+            childAspectRatio: 4,
+          ),
+          delegate: new SliverChildBuilderDelegate(
+              (BuildContext context, int index) {
+                return new Container(
+                  alignment: Alignment.center,
+                  color: Colors.teal[100 * (index % 9)],
+                  child: new Text('grid item $index'),
+                );
+              },
+              childCount: 20,
+            ),
+        ),
+        makeHeader('Header Section 4'),
+        SliverList(
+          delegate: SliverChildListDelegate(
+            [
+              Container(color: Colors.pink, height: 150.0),
+              Container(color: Colors.cyan, height: 150.0),
+              Container(color: Colors.indigo, height: 150.0),
+              Container(color: Colors.blue, height: 150.0),
+            ],
+          ),
+        ),
+      ],
     );
+  }
+}
+
+class _SliverAppBarDelegate extends SliverPersistentHeaderDelegate {
+  _SliverAppBarDelegate({
+    @required this.minHeight,
+    @required this.maxHeight,
+    @required this.child,
+  });
+
+  final double minHeight;
+  final double maxHeight;
+  final Widget child;
+
+  @override
+  double get minExtent => minHeight;
+
+  @override
+  double get maxExtent => math.max(maxHeight, minHeight);
+
+  @override
+  Widget build(BuildContext context, double shrinkOffset, bool overlapsContent) {
+    return new SizedBox.expand(child: child);
+  }
+
+  @override
+  bool shouldRebuild(_SliverAppBarDelegate oldDelegate) {
+    return maxHeight != oldDelegate.maxHeight ||
+      minHeight != oldDelegate.minHeight ||
+      child != oldDelegate.child;
   }
 }
 
